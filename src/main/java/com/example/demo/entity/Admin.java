44d73b1f35f2ef29e8e5c5070cb1a505b6163dd1
Maxim Tislenko <makstislenko@gmail.com>
2022-03-13 14:33:26 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/entity/Admin.java
@@ -0,0 +1,93 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "login")
+    private  String  login;
+    @Column(name = "password")
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(UUID id, String name, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return Objects.equals(id, admin.id) &&
+                Objects.equals(name, admin.name) &&
+                Objects.equals(login, admin.login) &&
+                Objects.equals(password, admin.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, login, password);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
